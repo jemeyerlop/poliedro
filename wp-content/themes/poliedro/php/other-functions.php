122e4b0de88b345c8de435dcf2463cc1cfcfe77b
Juan Enrique <jewebdeveloper@gmail.com>
2017-09-07 15:52:43 -0300

    menu function

--- wp-content/themes/poliedro/php/other-functions.php
+++ wp-content/themes/poliedro/php/other-functions.php
@@ -1,3 +1,12 @@
 <?php
-
+//MENU
+	//Self-managing navigation menu
+	add_theme_support( 'nav-menus' );
+	if ( function_exists( 'register_nav_menus' ) ) {
+		register_nav_menus(
+			array(
+			  'main_nav' => 'Main Navigation'
+			)
+		);
+	}
 ?>
